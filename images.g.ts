7a4a7f884fb9af2a0f4e03fd19980f28ac87c0cb
JavoAT <135654004+JavoAT@users.noreply.github.com>
2023-06-05 12:32:41 -0600

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Juan José":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . e e . . . . . . . . 
+. . . . . e e e e . . . . . . . 
+. . . . . e f e f . . . . . . . 
+. . . . . e e e e . . . . . . . 
+. . . . . c e e c . . . . . . . 
+. . . . c c c c c c . . . . 9 . 
+. . . c c c c c c c c . . 9 9 . 
+. . . e e c c c c e e . 9 9 9 . 
+. . e e . c c 1 c . e 9 9 9 . . 
+. . e e . c 1 1 1 c f e . . . . 
+. . . . . 8 8 . 8 8 . . . . . . 
+. . . . f f f . f f f . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
